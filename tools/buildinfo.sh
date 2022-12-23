5bf5de214e0ab8358a4c7c10626f14cdb14ad5d7
Jens Reidel <adrian@travitia.xyz>
2022-12-23 21:31:13 +0100

    build/make: Rename ARROW_DEVICE to KENVYRA_DEVICE
    Change-Id: I1b3454202b0de31e926ed667b3a4c910e64be237
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -58,6 +58,6 @@ if [ -n "$BUILD_THUMBPRINT" ] ; then
   echo "ro.build.thumbprint=$BUILD_THUMBPRINT"
 fi
 
-echo "ro.kenvyra.device=$ARROW_DEVICE"
+echo "ro.kenvyra.device=$KENVYRA_DEVICE"
 
 echo "# end build properties"
